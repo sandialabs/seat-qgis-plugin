16c6469d2ce3eebf1efd6e44ab83f8cbd64e7b1d
ssolson <ssolson@sandia.gov>
2023-11-17 14:44:18 -0700

    add pandas test

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,4 +1,7 @@
 import unittest
+from qgis.core import QgsApplication
+# Adjust the import path if necessary
+from seat.stressor_receptor_calc import classFactory
 
 
 class ExampleTest(unittest.TestCase):
@@ -6,36 +9,26 @@ class ExampleTest(unittest.TestCase):
         self.assertTrue(True)
 
 
+class TestPandasInstallation(unittest.TestCase):
+    def test_pandas_installed(self):
+        try:
+            import pandas
+            pandas_available = True
+        except ImportError:
+            pandas_available = False
+        self.assertTrue(pandas_available, "Pandas is not installed.")
+
+
 def run_all():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(ExampleTest))
+    suite.addTest(unittest.makeSuite(TestPandasInstallation))
     runner = unittest.TextTestRunner()
     runner.run(suite)
 
 
 if __name__ == '__main__':
     run_all()
-
-
-# import unittest
-# from qgis.core import QgsApplication
-# # Adjust the import path if necessary
-# from seat.stressor_receptor_calc import classFactory
-
-
-# class ExampleTest(unittest.TestCase):
-#     def test_basic(self):
-#         self.assertTrue(True)
-
-
-# class TestPandasInstallation(unittest.TestCase):
-#     def test_pandas_installed(self):
-#         try:
-#             import pandas
-#             pandas_available = True
-#         except ImportError:
-#             pandas_available = False
-#         self.assertTrue(pandas_available, "Pandas is not installed.")
 
 
 # class TestSeatPluginInstallation(unittest.TestCase):
