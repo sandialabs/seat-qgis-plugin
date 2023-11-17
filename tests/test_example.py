1e9c144b28e88eaee20424694af9c84c48f34553
ssolson <ssolson@sandia.gov>
2023-11-17 15:54:00 -0700

    pandas own test

--- tests/test_example.py
+++ tests/test_example.py
@@ -8,6 +8,8 @@ class ExampleTest(unittest.TestCase):
     def test_basic(self):
         self.assertTrue(True)
 
+
+class TestPandasInstallation(unittest.TestCase):
     def test_pandas_installed(self):
         try:
             import pandas
@@ -17,20 +19,10 @@ class ExampleTest(unittest.TestCase):
         self.assertTrue(pandas_available, "Pandas is not installed.")
 
 
-# class TestPandasInstallation(unittest.TestCase):
-#     def test_pandas_installed(self):
-#         try:
-#             import pandas
-#             pandas_available = True
-#         except ImportError:
-#             pandas_available = False
-#         self.assertTrue(pandas_available, "Pandas is not installed.")
-
-
 def run_all():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(ExampleTest))
-    # suite.addTest(unittest.makeSuite(TestPandasInstallation))
+    suite.addTest(unittest.makeSuite(TestPandasInstallation))
     runner = unittest.TextTestRunner()
     runner.run(suite)
 
