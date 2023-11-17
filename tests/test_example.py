a264f402d1403a106b93e4d12653dee6d8d741d6
ssolson <ssolson@sandia.gov>
2023-11-17 11:01:43 -0700

    try to run a real test

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,4 +1,22 @@
 import unittest
+from qgis.core import QgsApplication
+# Adjust the import path if necessary
+from seat.stressor_receptor_calc import classFactory
+
+
+class TestSeatPluginInstallation(unittest.TestCase):
+
+    def setUp(self):
+        self.qgis_app = QgsApplication([], False)
+        QgsApplication.initQgis()
+
+    def tearDown(self):
+        QgsApplication.exitQgis()
+
+    def test_plugin_initialization(self):
+        mock_iface = None  # Mock interface, adjust as needed
+        plugin_instance = classFactory(mock_iface)
+        self.assertIsNotNone(plugin_instance)
 
 
 class ExampleTest(unittest.TestCase):
@@ -9,6 +27,7 @@ class ExampleTest(unittest.TestCase):
 def run_all():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(ExampleTest))
+    suite.addTest(unittest.makeSuite(TestSeatPluginInstallation))
     runner = unittest.TextTestRunner()
     runner.run(suite)
 
