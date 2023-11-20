1fe0b219789fe77958f33975a331dc1abde0c90c
ssolson <ssolson@sandia.gov>
2023-11-20 11:35:58 -0700

    import seat

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,7 +1,7 @@
 import unittest
 from qgis.core import QgsApplication
 # Adjust the import path if necessary
-# from seat.stressor_receptor_calc import classFactory
+from seat.stressor_receptor_calc import classFactory
 
 
 class ExampleTest(unittest.TestCase):
@@ -29,31 +29,3 @@ def run_all():
 
 if __name__ == '__main__':
     run_all()
-
-
-# class TestSeatPluginInstallation(unittest.TestCase):
-
-#     def setUp(self):
-#         self.qgis_app = QgsApplication([], False)
-#         QgsApplication.initQgis()
-
-#     def tearDown(self):
-#         QgsApplication.exitQgis()
-
-#     def test_plugin_initialization(self):
-#         mock_iface = None  # Mock interface, adjust as needed
-#         plugin_instance = classFactory(mock_iface)
-#         self.assertIsNotNone(plugin_instance)
-
-
-# def run_all():
-#     suite = unittest.TestSuite()
-#     suite.addTest(unittest.makeSuite(ExampleTest))
-#     suite.addTest(unittest.makeSuite(TestSeatPluginInstallation))
-#     suite.addTest(unittest.makeSuite(TestPandasInstallation))
-#     runner = unittest.TextTestRunner()
-#     runner.run(suite)
-
-
-# if __name__ == '__main__':
-#     run_all()
