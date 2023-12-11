5b7cfcfbc83af3e0bed26a7fffef077d230bb106
ssolson <ssolson@sandia.gov>
2023-12-11 10:29:05 -0700

    qgis batch tests

---
+++ tests/qgis_tests.bat
@@ -0,0 +1,22 @@
+@echo off
+
+:: Set the QGIS installation path
+set "QGIS_ROOT=C:\Program Files\QGIS 3.34.1"
+
+:: Set PYTHONPATH to include QGIS Python modules
+set "PYTHONPATH=%QGIS_ROOT%\apps\qgis\python;%PYTHONPATH%"
+set "PYTHONHOME=%QGIS_ROOT%\apps\Python39"
+
+:: Set PATH to include QGIS binaries and libraries
+set "PATH=%QGIS_ROOT%\bin;%QGIS_ROOT%\apps\qgis\bin;%PATH%"
+
+:: Check if netCDF4 is installed and install if not
+"%QGIS_ROOT%\bin\python.exe" -m pip show netCDF4 >nul 2>&1
+if %errorlevel% neq 0 (
+    echo netCDF4 not found, installing...
+    "%QGIS_ROOT%\bin\python.exe" -m pip install netCDF4 ipdb
+)
+@REM "%QGIS_ROOT%\bin\python.exe" -m pip install ipdb
+
+:: Run your QGIS-based Python script
+"%QGIS_ROOT%\bin\python.exe" test_shear_stress.py
