4cdaa118409a37c00ae979d6c546e795a0a05e8b
ssolson <sterlingolson@yahoo.com>
2024-01-26 11:36:06 -0700

    add test for file probabilities selection

--- tests/test_stressor_receptor_calc.py
+++ tests/test_stressor_receptor_calc.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import tempfile
+import unittest
+from unittest.mock import MagicMock, patch
 from qgis.core import QgsApplication
 
 
@@ -28,11 +30,13 @@ class TestStressorReceptorCalcModule(unittest.TestCase):
         """
         Class method to set up test environment.
         """
-        # Other setup code...
-
         # Create a mock CSV file for testing
         cls.mock_csv_file = 'mock_style_file.csv'
         cls.create_mock_csv(cls.mock_csv_file)
+
+        cls.mock_iface = MagicMock()  # Mock the iface
+        cls.stressor_receptor_calc = sr.StressorReceptorCalc(cls.mock_iface)
+        cls.stressor_receptor_calc.dlg = MagicMock()
 
     @classmethod
     def tearDownClass(cls):
@@ -69,7 +73,30 @@ class TestStressorReceptorCalcModule(unittest.TestCase):
         
         pd.testing.assert_frame_equal(result_df, expected_df)
 
-    
+    @patch('seat.stressor_receptor_calc.QFileDialog.getOpenFileName')
+    def test_select_probabilities_file(self, mock_getOpenFileName):
+        """
+        Test the select_probabilities_file method.
+        """
+        # stressor_receptor_calc = sr.StressorReceptorCalc(None)  
+        # stressor_receptor_calc.dlg = 
+        # result_df = stressor_receptor_calc.read_style_files(self.mock_csv_file)
+
+        # Adjusting expected_df to have the same index name as result_df
+        # expected_df = pd.DataFrame({'Value': [10, 20, 30]}, index=pd.Index(['Type1', 'Type2', 'Type3'], name='Type'))
+        
+        # pd.testing.assert_frame_equal(result_df, expected_df)
+  
+
+        # mock object created to simulate the behavior of QFileDialog.getOpenFileName.
+        mock_filename="C:/Users/sterl/Codes/seat-qgis-plugin/tests/data/structured/probabilities/probabilities.csv"
+        mock_getOpenFileName.return_value = (mock_filename, None)
+
+        self.stressor_receptor_calc.select_probabilities_file()
+        # checks that the setText method of self.stressor_receptor_calc.dlg.  probabilities_file was called exactly once with mock_filename as its argument. 
+        self.stressor_receptor_calc.dlg.probabilities_file.setText.assert_called_once_with(mock_filename)
+
+
 
 def run_all():
     suite = unittest.TestSuite()
