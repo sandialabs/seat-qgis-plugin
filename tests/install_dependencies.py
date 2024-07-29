8366698eebff6717f4bf959170eb9150b4ef47e4
Christian <cjwinkler0@gmail.com>
2024-07-29 13:02:28 -0400

    add matplotlib to dependencies

--- tests/install_dependencies.py
+++ tests/install_dependencies.py
@@ -1,9 +1,8 @@
-# install_dependencies.py
 import subprocess
 import sys
 
 # Default path to the Python executable
-default_python_exe = "python3" 
+default_python_exe = "python3"
 # Use the provided path if given, otherwise use the default
 python_exe = sys.argv[1] if len(sys.argv) > 1 else default_python_exe
 
@@ -11,9 +10,8 @@ python_exe = sys.argv[1] if len(sys.argv) > 1 else default_python_exe
 subprocess.run([python_exe, "-m", "pip", "install", "--upgrade", "pip"], check=True)
 
 # List of packages to install
-packages = ["pandas", "netCDF4", "pytest"]
+packages = ["pandas", "netCDF4", "pytest", "matplotlib"]
 
 # Install each package using pip
 for package in packages:
     subprocess.run([python_exe, "-m", "pip", "install", package], check=True)
-
