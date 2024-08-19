36cd1efecb36f5437e1f47190a674f8d3723f0f5
Christian <cjwinkler0@gmail.com>
2024-08-19 10:16:51 -0400

    fix install dependencies

--- tests/install_dependencies.py
+++ tests/install_dependencies.py
@@ -6,12 +6,12 @@ default_python_exe = "python3"
 # Use the provided path if given, otherwise use the default
 python_exe = sys.argv[1] if len(sys.argv) > 1 else default_python_exe
 
-# Upgrade pip
-subprocess.run([python_exe, "-m", "pip", "install", "--upgrade", "pip"], check=True)
+# # Upgrade pip
+# subprocess.run([python_exe, "-m", "pip", "install", "--upgrade", "pip"], check=True)
 
-# List of packages to install
-packages = ["pandas", "netCDF4", "pytest",'rasterio']
+# # List of packages to install
+# packages = ["pandas", "netCDF4", "pytest",'rasterio']
 
-# Install each package using pip
-for package in packages:
-    subprocess.run([python_exe, "-m", "pip", "install", package], check=True)
+# # Install each package using pip
+# for package in packages:
+#     subprocess.run([python_exe, "-m", "pip", "install", package], check=True)
