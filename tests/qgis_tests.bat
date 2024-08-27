a2011c0f1caf4d9e1f6120b3bc4d5b0f2c8e1eff
Christian <cjwinkler0@gmail.com>
2024-08-27 11:13:57 -0400

    run all tests, original test_stressor_receptor_calc

--- tests/qgis_tests.bat
+++ tests/qgis_tests.bat
@@ -15,8 +15,8 @@ set "PATH=%QGIS_ROOT%\bin;%QGIS_ROOT%\apps\qgis-ltr\bin;%PATH%"
 @REM "%QGIS_ROOT%\bin\python.exe"  tests\install_dependencies.py "C:\\Program Files\\QGIS 3.34.9\\bin\\python.exe"
 
 :: Run pytest on all test scripts in the directory
-@REM "%QGIS_ROOT%\bin\python.exe" -m pytest .
+"%QGIS_ROOT%\bin\python.exe" -m pytest .
 @REM "%QGIS_ROOT%\bin\python.exe" -m pytest -s tests\test_shear_stress.py
 
-"%QGIS_ROOT%\bin\python.exe" tests\test_stressor_utils.py
+@REM "%QGIS_ROOT%\bin\python.exe" tests\test_stressor_receptor_calc.py
 
