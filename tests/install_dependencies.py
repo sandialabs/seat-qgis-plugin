79b82dad66100e501b08a12f33de9c6244aaf53c
Christian <cjwinkler0@gmail.com>
2024-08-29 13:12:22 -0400

    install pylint in yml file

--- tests/install_dependencies.py
+++ tests/install_dependencies.py
@@ -10,9 +10,9 @@ python_exe = sys.argv[1] if len(sys.argv) > 1 else default_python_exe
 # subprocess.run([python_exe, "-m", "pip", "install", "--upgrade", "pip"], check=True)
 
 # List of packages to install
-packages = ["pandas", "netCDF4", "pytest", "pylint"]
+# packages = ["pandas", "netCDF4", "pytest", "pylint"]
 
-# Install each package using pip
-for package in packages:
-    subprocess.run([python_exe, "-m", "pip", "install", package], check=True)
+# # Install each package using pip
+# for package in packages:
+#     subprocess.run([python_exe, "-m", "pip", "install", package], check=True)
 
