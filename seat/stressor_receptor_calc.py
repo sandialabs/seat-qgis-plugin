51100b82129121526a9e7cf9630aa5d9ae3c4529
Tim Nelson <tnelson@integral-corp.com>
2024-09-04 15:48:07 -0400

    update icon location for missing toolbar icon

--- seat/stressor_receptor_calc.py
+++ seat/stressor_receptor_calc.py
@@ -196,7 +196,7 @@ class StressorReceptorCalc:
     def initGui(self):
         """Create the menu entries and toolbar icons inside the QGIS GUI."""
 
-        icon_path = ":/plugins/seat/icon.png"
+        icon_path = ":/plugins/seat_qgis_plugin/icon.png"
         self.add_action(
             icon_path,
             setText=self.tr(
