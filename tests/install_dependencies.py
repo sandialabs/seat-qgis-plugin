fdfd638b8ae646b586a5c58e67257f144a7cf611
ssolson <sterlingolson@yahoo.com>
2024-09-24 11:41:11 -0400

    uncomment

--- tests/install_dependencies.py
+++ tests/install_dependencies.py
@@ -7,11 +7,11 @@ default_python_exe = "python3"
 python_exe = sys.argv[1] if len(sys.argv) > 1 else default_python_exe
 
 # Upgrade pip
-# subprocess.run([python_exe, "-m", "pip", "install", "--upgrade", "pip"], check=True)
+subprocess.run([python_exe, "-m", "pip", "install", "--upgrade", "pip"], check=True)
 
 # List of packages to install
-# packages = ["pandas", "netCDF4", "pytest", "pylint"]
+packages = ["pandas", "netCDF4", "pytest", "pylint"]
 
-# # Install each package using pip
-# for package in packages:
-#     subprocess.run([python_exe, "-m", "pip", "install", package], check=True)
+# Install each package using pip
+for package in packages:
+    subprocess.run([python_exe, "-m", "pip", "install", package], check=True)
