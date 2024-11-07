c063f528d5ec97aba3f55a8ae11e9ab9e8c8e6d2
Tim Nelson <tnelson@integral-corp.com>
2024-11-07 15:16:52 -0500

    updated comments

--- seat/paracousti_pre_process_routine.py
+++ seat/paracousti_pre_process_routine.py
@@ -1,3 +1,12 @@
+# Processing of parAcousti files can be resource instensive depending on the grid resolution and weights selected. This may take several hours to complete.
+# NOTES: 
+# Requires installation of xarray with netCDF4 (https://docs.xarray.dev/en/stable/getting-started-guide/installing.html)
+#     python -m pip install "xarray[io]"
+# Requires installation of scipy (https://scipy.org/install/)
+#     This will be included with "xarray[io]" or python -m pip install scipy
+# Requires intallation of tqdm (https://tqdm.github.io/)
+#     python -m pip install tqdm
+
 import os
 import utils.paracousti_fxns as paracousti_fxns
 import xarray as xr
