9984a5e273cc84d4014d9e91c7d83eb240606bc4
ssolson <sterlingolson@yahoo.com>
2025-02-12 13:13:12 -0500

    latest teests

--- tests/qgis_tests.bat
+++ tests/qgis_tests.bat
@@ -15,8 +15,7 @@ set "PATH=%QGIS_ROOT%\bin;%QGIS_ROOT%\apps\qgis-ltr\bin;%PATH%"
 @REM "%QGIS_ROOT%\bin\python.exe"  tests\install_dependencies.py "C:\\Program Files\\QGIS 3.34.9\\bin\\python.exe"
 
 :: Run pytest on all test scripts in the directory
-@REM "%QGIS_ROOT%\bin\python.exe" -m pytest .
-"%QGIS_ROOT%\bin\python.exe" -m pytest tests\test_stressor_receptor_calc.py
-@REM "%QGIS_ROOT%\bin\python.exe" -m pylint seat
-@REM "%QGIS_ROOT%\bin\python.exe" -m pylint seat/__init__.py
+"%QGIS_ROOT%\bin\python.exe" -m pytest .
+@REM "%QGIS_ROOT%\bin\python.exe" -m pytest tests\test_stressor_receptor_calc.py
+"%QGIS_ROOT%\bin\python.exe" -m pylint seat
 @REM "%QGIS_ROOT%\bin\python.exe" -m pylint seat/plugin_upload.py
